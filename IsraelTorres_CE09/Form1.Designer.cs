1c62279336f257caaf52c0571894f6ed4ba1168a
Israel Torres <israel.a.torres01@gmail.com>
2019-05-27 17:16:45 -0400

    First Commit

--- IsraelTorres_CE09/Form1.Designer.cs
+++ IsraelTorres_CE09/Form1.Designer.cs
@@ -28,13 +28,274 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxGenre = new System.Windows.Forms.TextBox();
+            this.numericUpDownYear = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownPrice = new System.Windows.Forms.NumericUpDown();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.recordStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLast);
+            this.groupBox1.Controls.Add(this.btnNext);
+            this.groupBox1.Controls.Add(this.btnPrev);
+            this.groupBox1.Controls.Add(this.btnFirst);
+            this.groupBox1.Controls.Add(this.numericUpDownPrice);
+            this.groupBox1.Controls.Add(this.numericUpDownYear);
+            this.groupBox1.Controls.Add(this.textBoxGenre);
+            this.groupBox1.Controls.Add(this.textBoxTitle);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1132, 510);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DVD Information";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1156, 42);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 38);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Genre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Year:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(161, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Price:";
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(234, 76);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.ReadOnly = true;
+            this.textBoxTitle.Size = new System.Drawing.Size(698, 31);
+            this.textBoxTitle.TabIndex = 4;
+            // 
+            // textBoxGenre
+            // 
+            this.textBoxGenre.Location = new System.Drawing.Point(234, 143);
+            this.textBoxGenre.Name = "textBoxGenre";
+            this.textBoxGenre.ReadOnly = true;
+            this.textBoxGenre.Size = new System.Drawing.Size(406, 31);
+            this.textBoxGenre.TabIndex = 5;
+            // 
+            // numericUpDownYear
+            // 
+            this.numericUpDownYear.Location = new System.Drawing.Point(234, 215);
+            this.numericUpDownYear.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownYear.Name = "numericUpDownYear";
+            this.numericUpDownYear.ReadOnly = true;
+            this.numericUpDownYear.Size = new System.Drawing.Size(204, 31);
+            this.numericUpDownYear.TabIndex = 6;
+            // 
+            // numericUpDownPrice
+            // 
+            this.numericUpDownPrice.DecimalPlaces = 2;
+            this.numericUpDownPrice.Location = new System.Drawing.Point(234, 287);
+            this.numericUpDownPrice.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownPrice.Name = "numericUpDownPrice";
+            this.numericUpDownPrice.ReadOnly = true;
+            this.numericUpDownPrice.Size = new System.Drawing.Size(204, 31);
+            this.numericUpDownPrice.TabIndex = 7;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFirst.Location = new System.Drawing.Point(234, 387);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(150, 60);
+            this.btnFirst.TabIndex = 8;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrev.Location = new System.Drawing.Point(415, 387);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(150, 60);
+            this.btnPrev.TabIndex = 9;
+            this.btnPrev.Text = "< Prev";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(601, 387);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(150, 60);
+            this.btnNext.TabIndex = 10;
+            this.btnNext.Text = "Next >";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLast.Location = new System.Drawing.Point(782, 387);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(150, 60);
+            this.btnLast.TabIndex = 11;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.recordStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 549);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1156, 37);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // recordStatus
+            // 
+            this.recordStatus.Name = "recordStatus";
+            this.recordStatus.Size = new System.Drawing.Size(93, 33);
+            this.recordStatus.Text = "Record:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1156, 586);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Movies on DVD";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrice;
+        private System.Windows.Forms.NumericUpDown numericUpDownYear;
+        private System.Windows.Forms.TextBox textBoxGenre;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel recordStatus;
     }
 }
 
--- IsraelTorres_CE09/Form1.cs
+++ IsraelTorres_CE09/Form1.cs
@@ -7,14 +7,241 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Xml;
+
+/*
+ * Name: Israel Torres
+ * Class: Visual Frameworks - Online (MDV1830-O)
+ * Term: C201905 01
+ * Code Exercise: Database Connectivity
+ * Number: CE09
+ */
 
 namespace IsraelTorres_CE09
 {
     public partial class Form1 : Form
     {
+        // connection string variable
+        MySqlConnection conn = new MySqlConnection();
+
+        DataTable theData = new DataTable();
+
+        // variable to for number of records
+        int numberOfRecords = 0;
+
+        // variable for the current record
+        int record = 0;
+
+        // variable for the current row
+        int row = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            string connetionString = BuildConnectionString("exampleDatabase", "dbsAdmin", "password");
+
+            Connect(connetionString);
+
+            RetrieveData();
+        }
+
+        private bool RetrieveData()
+        {
+            // create the SQL statement
+            string sql = "SELECT DVD_Title, Price, Year, Genre FROM dvd LIMIT 25";
+
+            // Create the DataAdapter
+            MySqlDataAdapter adr = new MySqlDataAdapter(sql, conn);
+
+            adr.SelectCommand.CommandType = CommandType.Text;
+
+            adr.Fill(theData);
+
+            // get a count of the number of rows within the DataTable
+            numberOfRecords = theData.Select().Length;
+
+            // put the first record's data into the form
+            textBoxTitle.Text = theData.Rows[0]["DVD_Title"].ToString();
+            textBoxGenre.Text = theData.Rows[0]["Genre"].ToString();
+            numericUpDownYear.Value = Convert.ToDecimal(theData.Rows[0]["Year"]);
+            numericUpDownPrice.Value = Convert.ToDecimal(theData.Rows[0]["Price"]);
+
+            recordStatus.Text = "Record 1 of " + numberOfRecords;
+
+            return true;
+        }
+
+        private void Connect(string myConnectionString)
+        {
+            try
+            {
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                //MessageBox.Show("Connected!");
+            }
+            catch (MySqlException e)
+            {
+
+                // message strig variable
+                string msg = "";
+
+                // check what exception was received
+                switch (e.Number)
+                {
+                    case 0:
+                        msg = e.ToString();
+                        break;
+                    case 1042:
+                        msg = "Can't resolve host address.\n" + myConnectionString;
+                        break;
+                    case 1045:
+                        msg = "invalid username/password";
+                        break;
+                    default:
+                        // generic message if the others don't cover it
+                        msg = e.ToString() + "\n" + myConnectionString;
+                        break;
+                }
+                MessageBox.Show(msg);
+            }
+        }
+
+        private string BuildConnectionString(string database, string uid, string pwd)
+        {
+            string serverIP = "";
+
+            try
+            {
+                // open the text file using stream reader
+                using (StreamReader sr = new StreamReader("C:\\VFW\\connect.txt"))
+                {
+                    // reader the server IP data
+                    serverIP = sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+            return "server=" + serverIP + ";uid=" + uid + ";pwd=" + pwd + ";database=" + database + " ;port=8889";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            row = 0;
+
+            textBoxTitle.Text = theData.Rows[row]["DVD_Title"].ToString();
+            textBoxGenre.Text = theData.Rows[row]["Genre"].ToString();
+            numericUpDownYear.Value = Convert.ToDecimal(theData.Rows[row]["Year"]);
+            numericUpDownPrice.Value = Convert.ToDecimal(theData.Rows[row]["Price"]);
+
+            record = row + 1;
+
+            recordStatus.Text = "Record " + record + " of " + numberOfRecords;
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            row = 24;
+
+            textBoxTitle.Text = theData.Rows[row]["DVD_Title"].ToString();
+            textBoxGenre.Text = theData.Rows[row]["Genre"].ToString();
+            numericUpDownYear.Value = Convert.ToDecimal(theData.Rows[row]["Year"]);
+            numericUpDownPrice.Value = Convert.ToDecimal(theData.Rows[row]["Price"]);
+
+            record = row + 1;
+
+            recordStatus.Text = "Record " + record + " of " + numberOfRecords;
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (row > 0)
+            {
+                row--;
+
+                textBoxTitle.Text = theData.Rows[row]["DVD_Title"].ToString();
+                textBoxGenre.Text = theData.Rows[row]["Genre"].ToString();
+                numericUpDownYear.Value = Convert.ToDecimal(theData.Rows[row]["Year"]);
+                numericUpDownPrice.Value = Convert.ToDecimal(theData.Rows[row]["Price"]);
+
+                record = row + 1;
+
+                recordStatus.Text = "Record " + record + " of " + numberOfRecords;
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (row + 1 < theData.Select().Length)
+            {
+                row++;
+
+                textBoxTitle.Text = theData.Rows[row]["DVD_Title"].ToString();
+                textBoxGenre.Text = theData.Rows[row]["Genre"].ToString();
+                numericUpDownYear.Value = Convert.ToDecimal(theData.Rows[row]["Year"]);
+                numericUpDownPrice.Value = Convert.ToDecimal(theData.Rows[row]["Price"]);
+
+                record = row + 1;
+
+                recordStatus.Text = "Record " + record + " of " + numberOfRecords;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.DefaultExt = "xml";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // instantiate an XmlWriterSettings object
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.ConformanceLevel = ConformanceLevel.Document;
+
+                // set the indent to true
+                settings.Indent = true;
+
+                // create the XmlWriter
+                using (XmlWriter writer = XmlWriter.Create(saveFileDialog.FileName, settings))
+                {
+                    if (theData.Rows.Count > 0)
+                    {
+                        // write the unique indentifier
+                        writer.WriteStartElement("dvd_info");
+
+                        for (int i = 0; i < theData.Select().Length; i++)
+                        {
+                            // write the unique indentifier
+                            writer.WriteStartElement("dvd");
+
+                            writer.WriteElementString("DVD_Title", theData.Rows[i]["DVD_Title"].ToString());
+                            writer.WriteElementString("Genre", theData.Rows[i]["Genre"].ToString());
+                            writer.WriteElementString("Year", theData.Rows[i]["Year"].ToString());
+                            writer.WriteElementString("Price", theData.Rows[i]["Price"].ToString());
+
+                            writer.WriteEndElement();
+                        }
+
+                        writer.WriteEndElement();
+                    }
+                    else
+                    {
+                        MessageBox.Show("There is no data to save.");
+                    }
+
+                }
+            }
         }
     }
 }
